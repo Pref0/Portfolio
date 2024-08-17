ed99fc3d5d4a894c5ebd6cb9ea4669fa7c3253b3
Pref0 <ttalaoja@gmail.com>
2024-08-17 21:25:10 +0300

    delete - Palvelin X

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,16 +1,8 @@
 import { Project } from "./interfaces";
-import PalvelinX from "../assets/palvelinx_pink.png";
 import Discord from "../assets/Discord.png";
 import Portfolio from "../assets/portfolio.png";
 
 export const projects: Project[] = [
-  {
-    name: "Palvelin X",
-    description: "Finnish FiveM server",
-    github: "",
-    website: "",
-    imageUrl: PalvelinX,
-  },
   {
     name: "Discord Bot with AI",
     description: "Little project made for fun",
